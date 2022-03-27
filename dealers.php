ff50d9e4d740792295dc0817e58057f2bd59a33e
raops01 <raops01@pfw.edu>
2022-03-27 01:51:06 -0400

    basic ui concept

---
+++ dealers.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <title>Dealers</title>
+    <span id="logo"></span>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+    <div class="topnav">
+        <a href="dealers.php">Dealers</a>
+        <a href="transaction.php">Buy a Car</a>
+        <a href="index.php">Main Page</a>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <title>Claim a Car</title>
+        <span id="logo"><a href="index.php"></span>
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+    </head>
+
+    <body>
+        <div class="topnav">
+            <a href="dealers.php">Dealers</a>
+            <a href="transaction.php">Buy a Car</a>
+            <a href="index.php">Main Page</a>
+        </div>
+    </body>
+</html>
